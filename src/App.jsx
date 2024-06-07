8f2d7092f3cb3b65e177311c58cd18751707b910
常峰 <8845317+winlatom@user.noreply.gitee.com>
2024-06-07 18:57:17 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation
+} from "react-router-dom";
+import { ConfigProvider } from 'zarm';
+import routes from '@/router';
+import NavBar from '@/components/NavBar';
+
+const  App = () => {
+  const location = useLocation()
+  const { pathname } = location // 获取当前路径
+  const needNav = ['/', '/data', '/user'] // 需要底部导航栏的路径
+  const [showNav, setShowNav] = useState(false) // 是否展示 Nav
+  useEffect(() => {
+    setShowNav(needNav.includes(pathname))
+  }, [pathname]) // [] 内的参数若是变化，便会执行上述回调函数=
+  return <ConfigProvider primaryColor={'#007fff'}>
+    <>
+      <Routes>
+        {routes.map(route => <Route exact key={route.path} path={route.path} element={<route.component />} />)}
+      </Routes>
+      <NavBar showNav={showNav} />
+    </>
+  </ConfigProvider>;
+};
+
+export default App;
---
+++ src/container/User/index.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Cell, Modal, Input, Button, Toast, FilePicker } from 'zarm';
+import { get, post, imgUrlTrans } from '@/utils';
+
+import s from './style.module.less';
+
+const User = () => {
+  const navigateTo = useNavigate();
+  const [user, setUser] = useState({});
+  const [signature, setSignature] = useState('');
+  const [show, setShow] = useState(false);
+  const [avatar, setAvatar] = useState('');
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  // 获取用户信息
+  const getUserInfo = async () => {
+    const { data } = await get('/api/user/get_userinfo');
+    setUser(data);
+    setAvatar(imgUrlTrans(data.avatar))
+    setSignature(data.signature)
+  };
+
+  // 个性签名弹窗确认
+  const confirmSig = async () => {
+    const { data } = await post('/api/user/edit_signature', {
+      signature: signature
+    });
+    setUser(data);
+    setShow(false);
+    Toast.show('修改成功');
+  } ;
+
+  // 退出登录
+  const logout = async () => {
+    localStorage.removeItem('token');
+    navigateTo('/login');
+  };
+
+  return <div className={s.user}>
+    <div className={s.head}>
+      <div className={s.info}>
+        <span>昵称：{ user.username }</span>
+        <span>
+          <img style={{ width: 30, height: 30, verticalAlign: '-10px' }} src="//s.yezgea02.com/1615973630132/geqian.png" alt="" />
+          <b>{ user.signature || '暂无内容' }</b>
+        </span>
+      </div>
+      <img className={s.avatar} style={{ width: 60, height: 60, borderRadius: 8 }} src={avatar} alt="" />
+   </div>
+   <div className={s.content}>
+    <Cell
+      hasArrow
+      title="用户信息修改"
+      onClick={() => navigateTo('/userinfo')}
+      icon={<img style={{ width: 20, verticalAlign: '-7px' }} src="//s.yezgea02.com/1615974766264/gxqm.png" alt="" />}
+    />
+    <Cell
+      hasArrow
+      title="重制密码"
+      onClick={() => navigateTo('/account')}
+      icon={<img style={{ width: 20, verticalAlign: '-7px' }} src="//s.yezgea02.com/1615974766264/zhaq.png" alt="" />}
+    />
+    {/* <Cell
+      hasArrow
+      title="我的标签"
+      icon={<img style={{ width: 20, verticalAlign: '-7px' }} src="//s.yezgea02.com/1619321650235/mytag.png" alt="" />}
+    /> */}
+    <Cell
+      hasArrow
+      title="关于我们"
+      onClick={() => navigateTo('/about')}
+      icon={<img style={{ width: 20, verticalAlign: '-7px' }} src="//s.yezgea02.com/1615975178434/lianxi.png" alt="" />}
+    />
+   </div>
+   <Button className={s.logout} block theme="danger" onClick={logout}>退出登录</Button>
+   <Modal
+      visible={show}
+      title="标题"
+      closable
+      onCancel={() => setShow(false)}
+      footer={
+        <Button block theme="primary" onClick={confirmSig}>
+          确认
+        </Button>
+      }
+    >
+    <Input
+        autoHeight
+        showLength
+        maxLength={50}
+        type="text"
+        rows={3}
+        value={signature}
+        placeholder="请输入备注信息"
+        onChange={(val) => setSignature(val)}
+        />
+    </Modal>
+  </div>
+};
+
+export default User;
