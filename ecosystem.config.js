9b23880aeb3214190f67658eecff93ef88f12083
changfeng <1610618004@qq.com>
2024-06-07 21:35:37 +0800

    feat: config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'juejue-vite-h5',
+      name: 'coinkeeper-fe',
       script: 'juejue-vite-h5-server.js'
     },
   ],
   deploy: {
     production: {
       user: 'root',
-      host: '47.99.134.126',
+      host: '101.201.108.246',
       ref: 'origin/master',
-      repo: 'git@git.zhlh6.cn:Nick930826/juejue-vite-h5.git',
-      path: '/workspace/juejue-vite-h5',
+      repo: 'git@github.com:Feweb-wind/coinkeeper-fe.git',
+      path: '/workspace/coinkeeper-fe',
       'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
         NODE_ENV: 'production'
