4191a2ae07e497a158125be38cc9031fb589fbfb
changfeng <1610618004@qq.com>
2024-06-07 22:27:40 +0800

    feat: config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:Feweb-wind/coinkeeper-fe.git',
       path: '~/workspace/coinkeeper-fe',
-      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
         NODE_ENV: 'production'
       }
